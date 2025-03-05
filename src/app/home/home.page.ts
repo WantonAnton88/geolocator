6350b5795667a3955461546a34f5069bdccdc2f4
WantonAnton88 <antoniolllbono@gmail.com>
2025-03-05 15:45:42 +0800

    GeoLocator
    Done

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+  location: any;
+
+  constructor() {}
+
+  async getCurrentLocation() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      const { latitude, longitude } = coordinates.coords
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    } catch (error) {
+      alert("Error getting location: "+error);
+    }
+  }
+
+  async watchLocation() {
+    const watchId = Geolocation.watchPosition({}, (position, err) => {
+      if(err) {
+        console.log("Error watching position: ", err);
+        return
+      }
+      console.log("Watched Position: ", position);
+      this.location = position?.coords;
+    });
+  }
+}
